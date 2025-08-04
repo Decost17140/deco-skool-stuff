b99f5d951730d841692660d213b396e736ad9a96
Decost17140 <112478392+Decost17140@users.noreply.github.com>
2025-08-04 14:25:41 +0700

    Update 04_8_2568.py

--- 04_8_2568.py
+++ 04_8_2568.py
@@ -1,5 +1,4 @@
-def sum_of_square():
-    a = [50, 500, 5000]
-    sum = ((a[0])**2) + ((a[1])**2) + ((a[2])**2)
+def sum_of_square(a1,a2,a3):
+    sum = ((a1)**2) + ((a2)**2) + ((a3)**2)
     return sum
-print(sum_of_square())
+print(sum_of_square(50, 500, 5000))
